2396af928d3de0fd350b16f713adf49a2152d6c1
srayo16 <srayosikder@gmail.com>
2022-03-01 03:06:01 +0600

    commit again

--- js/index.js
+++ js/index.js
@@ -43,6 +43,24 @@ let IDphone = detail =>{
     .then(data => showDetails(data.data))
 }
 
-let showDetails = phone => {
-    console.log(phone);
+let showDetails = phones => {
+    console.log(phones);
+    let searchShow = document.getElementById('searchshow');
+    searchShow.textContent = '';
+    
+    // phones.forEach(phone => {
+    let div = document.createElement('div');
+    div.classList.add('col');
+    div.innerHTML = `<div class="card img-fluid w-75 p-3">
+    <img src="${phones.image}" class="card-img-top" alt="">
+    <div class="card-body">
+      <h5 class="card-title">${phones.name}</h5>
+      <p class="card-text">${phones.releaseDate}</p>
+      <p class="card-text">Main features: ${phones.mainFeatures.chipSet} <br> ${phones.mainFeatures.displaySize} <br> ${phones.mainFeatures.memory} </p>
+      <p class="card-text">Sensors: ${phones.mainFeatures.sensors}</p>
+      <p class="card-text">Others: ${phones.others.Bluetooth} , ${phones.others.GPS} , ${phones.others.NFC} , ${phones.others.Radio} , ${phones.others.USB} , ${phones.others.WLAN}</p>
+    </div>
+  </div>`;
+  searchShow.appendChild(div);
+    // })
 } 
